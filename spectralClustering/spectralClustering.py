ec3a0da211b1ac6caa5815984e4d4e6b37b88015
Raghu Prabhakar <raghu@tflop1.stanford.edu>
2013-12-03 00:12:05 -0800

    Added logic to return the list of files that belong to each cluster. The "runClustering" method returns a list of file lists, one list per cluster

--- spectralClustering/spectralClustering.py
+++ spectralClustering/spectralClustering.py
@@ -8,29 +8,38 @@ from sklearn.cluster import spectral_clustering
 def readFile(f, delim=" "):
     retArray = []
     fHandle = open(f, "r")
-    fHandle.readline()
+    line = fHandle.readline()
+    fileList = line.split()
     csvReader = csv.reader(fHandle, delimiter=delim)
     for row in csvReader:
         if len(row) > 0:
-            print row
             retArray.append([float(x) for x in row[:-1]])
-    return retArray
+    return (retArray, fileList)
 
 
-def main():
+def runClustering():
     if len(sys.argv) != 3:
         print "Usage: %s <csv file containing correlation matrix> <num. clusters>\n" %(sys.argv[0])
         sys.exit(-1)
 
     fileName = sys.argv[1]
     numClusters = int(sys.argv[2])
-    contents = readFile(fileName, "\t")
-    print contents
+    (contents, fileList) = readFile(fileName, "\t")
 
     data = np.array(contents)
     labels = spectral_clustering(data, numClusters)
-    print labels
-    print "Length: %d" %(len(labels))
+#    print labels
+#    print "Length: %d" %(len(labels))
+    returnList = []
+    for cluster in range(numClusters):
+        clusterContents = filter(lambda x:labels[x] == cluster,range(len(labels)))
+        clusterFiles = [fileList[i] for i in clusterContents]
+        returnList.append(clusterFiles)
+
+    return returnList
 
 
-main()
+fileList = runClustering()
+for l in fileList:
+    print l
+
