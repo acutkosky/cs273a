d7ba6724c758e8c31e1b1182ba432e91f3d7711c
Ashok Cutkosky <ashok@dn0a23149f.sunet>
2013-12-04 00:53:17 -0800

    added a matrix

--- doIt.py
+++ doIt.py
@@ -24,7 +24,26 @@ def main():
         pref = "H1NonBinTSVs/TRANS2"
         training = clusterFiles[:]
         training = [pref + x for x in training]
-        test = choice(training)
+        maxscore = 0.0
+        besttest= ""
+        for test in training:
+            realtraining = training[:]
+            realtraining.remove(test)
+            X= read_files.GetMatrix(realtraining,"chr1")
+            Y = read_files.makevector(test,"chr1")
+            
+            if len(Y) > len(X):
+                Y = Y[:len(X)]
+            elif len(Y) < len(X):
+                Y = Y + [0]*(len(X)-len(Y))
+            clf = linearRegression.linearRegression(X,Y)
+            score = clf.score(X,Y)
+            if(score >maxscore):
+                maxscore = score
+                besttest = test
+
+
+        test = besttest
         training.remove(test)
         X = read_files.GetMatrix(training, "chr1")
         Y = read_files.makevector(test, "chr1")
@@ -39,9 +58,13 @@ def main():
         clf = linearRegression.linearRegression(X,Y)
         score = clf.score(X,Y)
         print "Predicting "+test[len(pref):-len(".wip")]
+        print "From:"
+        l = [x[len(pref):-len(".wip")] for x in training]
+        for thing in l:
+            print thing
         print "Linear regression R^2 score: %f" %(score)
         print "Linear regression RMS error: %f" %(read_files.RMSerror(clf,X,Y))
         print "Linear regression coefficients:"
-        print clf.coef_
+        print clf.coef_,clf.intercept_
 
 main()
